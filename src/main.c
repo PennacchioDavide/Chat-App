34d84e9fc7f0ec7243e0868ea2c914b6cd117b5f
PennacchioDavide <dapennacchio@hotmail.com>
2025-02-06 09:09:31 +0100

    creating project structure

--- src/main.c
+++ src/main.c
@@ -1,37 +1,30 @@
 #include <gtk/gtk.h>
+#include "server.h"
 
 int main(int argc, char *argv[])
 {
-    gtk_init(&argc, &argv);
+    gtk_init(&argc, &argv); // Init GTK
     
-    GtkWidget *window;
-    GtkWidget *button1;
-    GtkWidget *button2;
-    GtkWidget *box;
+    GtkWidget *window; // Declaring Window
+    GtkWidget *box; // Declaring Container Box
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL); // Init Window
 
-    button1 = gtk_button_new_with_label("Button 1");
-    button2 = gtk_button_new_with_label("Button 2");
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0); // Init Container Box
 
-    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add(GTK_CONTAINER (window), box); // Adding Container Box to Window
 
-    gtk_container_add(GTK_CONTAINER (window), box);
+    gtk_window_set_default_size(GTK_WINDOW (window), 1280, 720); // Setting Window Size
 
-    gtk_box_pack_start(GTK_BOX (box), button1, FALSE, FALSE, 50);
-    gtk_box_pack_start(GTK_BOX (box), button2, FALSE, FALSE, 50);
+    gtk_window_set_title(GTK_WINDOW (window), "Chat App"); // Setting Window Title
 
-    gtk_window_set_default_size(GTK_WINDOW (window), 1280, 720);
+    gtk_window_set_resizable(GTK_WINDOW (window), FALSE); // Setting Window Not Resizable
 
-    gtk_window_set_title(GTK_WINDOW (window), "Chat App");
+    g_signal_connect(window, "destroy", gtk_main_quit, NULL); // Enabling x Button
 
-    gtk_window_set_resizable(GTK_WINDOW (window), FALSE);
+    gtk_widget_show_all(window); // Show Window
 
-    g_signal_connect(window, "destroy", gtk_main_quit, NULL);
-
-    gtk_widget_show_all(window);
-
-    gtk_main();
+    gtk_main(); // Run GTK
 
     return 0;
 }
