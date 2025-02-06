1a938ab877e7e026e3d86dbf9f9cd9a181305545
PennacchioDavide <dapennacchio@hotmail.com>
2025-02-06 15:12:48 +0100

    basic gui structure

--- src/main.c
+++ src/main.c
@@ -2,11 +2,18 @@
 #define RAYGUI_IMPLEMENTATION
 #include "../include/raygui.h"
 #include "../include/server.h"
+#include <stdbool.h>
+#include <string.h>
 
 int main()
 {
     InitWindow(1280, 720, "Chat App");
     SetTargetFPS(60);
+
+    char buff[256] = "";
+    char text[256] = "";
+
+    bool showText = false;
 
     while (!WindowShouldClose())
     {
@@ -14,10 +21,24 @@ int main()
             
             ClearBackground(DARKGRAY);
 
-            DrawText("Contacts", 20, 20, 30, RAYWHITE);
+            DrawText("Chats:", 20, 20, 30, RAYWHITE);
 
-            DrawLine(200, 720, 200, 0, RAYWHITE);
-            DrawLine(0, 70, 200, 70, RAYWHITE);
+            DrawLine(200, 720, 200, 0, RAYWHITE); // Vertical Line
+            DrawLine(0, 70, 200, 70, RAYWHITE); // Horizontal Line
+
+            GuiTextBox((Rectangle){220, 660, 900, 30}, buff, 100, true);
+            
+            if((IsKeyPressed(KEY_ENTER) || GuiButton((Rectangle){1140, 660, 100, 30}, "SEND")) && strcmp(buff, "") != 0)
+            {
+                showText = true;
+                strcpy(text, buff);
+                strcpy(buff, "");
+            }
+
+            if(showText)
+            {
+                DrawText(text, 220, 100, 30, RAYWHITE);
+            }
 
         EndDrawing();
     }
